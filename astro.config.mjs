03974aee1585118f691f42f0bc64c072d6f7d8f9
pauline nemchak <doublewebstandards@gmail.com>
2024-12-13 17:52:51 +0000

    updated base url and links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://paulinenemchak.github.io',
-    base: '/dnd-5e-campaigns/',
+    base: '/dnd-5e-campaigns'
 });
